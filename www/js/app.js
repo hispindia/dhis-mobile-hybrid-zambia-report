ceea03b82b757d4f77d3ee69f7c150e4fd0dedc2
Nhan Cao <nhancv1992@gmail.com>
2016-03-18 14:31:26 +0530

    create dhis.compress

--- www/js/app.js
+++ www/js/app.js
@@ -6,8 +6,7 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('app', ['ionic', 'app.models', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'app.configs',
-    'd2Filters',
-    'd2Services',
+    'dhis2.compress',
     'd2TrackerServices',
     'pascalprecht.translate'
   ])
